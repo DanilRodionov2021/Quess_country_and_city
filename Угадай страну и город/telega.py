6ef6ef0ddba392d89cc4f4da43bdff270dc4cf43
DanilRodionov2021 <dan1rodionov@yandex.ru>
2022-05-10 14:43:51 +0300

    Угадай страну и город

--- Угадай страну и город/telega.py
+++ Угадай страну и город/telega.py
@@ -5,6 +5,7 @@ bot = telebot.TeleBot('5160177317:AAE10XVzZpo1d-JhkBtTTcrQlKMfQUHuRi0');
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
     bot.reply_to(message, "Здравствуй, мой юный географ! Вы готовы к суперигре УГАДАЙ СТРАНУ И ГОРОД?")
+    # начало беседы
  
 @bot.message_handler(func=lambda n: True) 
 def echo_all(message):
@@ -12,6 +13,7 @@ def echo_all(message):
         bot.reply_to(message, 'Ну что ж, тогда начинаем игру! Перейдите по ссылке на первый вопрос: http://t.me/Dan4ix_bot.')
     else:
         bot.reply_to(message, 'Ну что ж, тогда всего хорошего!')
+        # при положительном ответе начинается игра
      
  
 bot.polling()
--- Угадай страну и город/telega3.py
+++ Угадай страну и город/telega3.py
@@ -5,14 +5,16 @@ bot = telebot.TeleBot("5275373837:AAESq6Nb5iCkLap0_3zVFoVWGY8_-jVToBY")
  
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message): 
- bot.reply_to(message, "Здравствуй, участник игры! Второй вопрос! https://s1.1zoom.ru/b5050/453/Warsaw_Poland_Houses_Evening_512709_3840x2400.jpg Угадай страну и город!")
- 
+    bot.reply_to(message, "Здравствуй, участник игры! Второй вопрос! https://s1.1zoom.ru/b5050/453/Warsaw_Poland_Houses_Evening_512709_3840x2400.jpg Угадай страну и город!")
+    # второй вопрос
+
 @bot.message_handler(func=lambda n: True) 
 def echo_all(message):
- if message.text == 'Польша, Варшава':
-     bot.reply_to(message, 'Поздравляю, вы правильно ответили на второй вопрос! Перейдите по ссылке на третий вопрос: http://t.me/Kiev_Minsk_bot')
- else:
-     bot.reply_to(message, 'К сожалению, вы не ответили на второй вопрос. Будем надеяться, что ответите на последний вопрос! Перейдите по ссылке на последний вопрос: http://t.me/Kiev_Minsk_bot ') 
-     
+    if message.text == 'Польша, Варшава':
+        bot.reply_to(message, 'Поздравляю, вы правильно ответили на второй вопрос! Перейдите по ссылке на третий вопрос: http://t.me/Kiev_Minsk_bot')
+    else:
+        bot.reply_to(message, 'К сожалению, вы не ответили на второй вопрос. Будем надеяться, что ответите на последний вопрос! Перейдите по ссылке на последний вопрос: http://t.me/Kiev_Minsk_bot ')
+        # ответ на второй вопрос
+
  
 bot.polling()
